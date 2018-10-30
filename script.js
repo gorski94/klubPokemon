980f7cde64229e8511b7bc0585478a41c3b62b44
Michał Stanecki <michal.stanecki@coig.pl>
2018-10-30 16:19:35 +0100

    Zakladeczka i content

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function Position(){
+  var zakladeczka = document.getElementById('Zakladeczka');
+  zakladeczka.addEventListener('click', OnZakladeczkaClick);
     var ustawienieOkna = document.getElementById('panelButton');
     var szerokoscOkna = window.innerWidth;
     var wysokoscOkna = window.innerHeight;
@@ -7,4 +9,9 @@ function Position(){
     var srodekWysokosci = (wysokoscOkna/2) - (ustawienieOkna.clientHeight/2);
     ustawienieOkna.style.right = pozycja+"px";
     ustawienieOkna.style.bottom = srodekWysokosci+"px";
+  }
+
+function OnZakladeczkaClick(){  
+  document.getElementById("content").innerHTML = '';
+  document.getElementById("content").innerHTML='<object type="text/html" data="tabelka.html" ></object>';
 }
