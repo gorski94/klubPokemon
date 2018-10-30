fd51ab7d7e93baa5e4637c8c85c9667c40cd6106
Rafał Góral <rafal.goral@coig.pl>
2018-10-30 16:01:44 +0100

    Dodanie stałego miejsca na buttony

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function Position(){
-    var ustawienieOkna = document.getElementById('panelButton');
+    var ustawienieOkna = document.getElementById('middle-buttons');
     var szerokoscOkna = window.innerWidth;
     var wysokoscOkna = window.innerHeight;
 
     var pozycja =(szerokoscOkna/2) -(ustawienieOkna.clientWidth/2);
-    var srodekWysokosci = (wysokoscOkna/2) - (ustawienieOkna.clientHeight/2);
+    var srodekWysokosci = (wysokoscOkna/3) - (ustawienieOkna.clientHeight/2);
     ustawienieOkna.style.right = pozycja+"px";
     ustawienieOkna.style.bottom = srodekWysokosci+"px";
+	console.log('dupa');
 }
